4ae4e0a670e02e34e20a8c6e013459a280ca5642
PPranesh <ppranesh5@gmail.com>
2020-04-16 17:49:11 +0530

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,67 @@
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+[![Issues](https://img.shields.io/github/issues/PPranesh/Simple-Dynamic-Website)](https://github.com/PPranesh/My_ToDo_version_1/issues)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![Last Commit](https://img.shields.io/github/last-commit/PPranesh/Simple-Dynamic-Website)]()
+[![Read the Docs](https://readthedocs.org/projects/yt2mp3/badge/?version=latest)](https://yt2mp3.readthedocs.io/en/latest/?badge=latest)
+[![Code of Conduct](https://img.shields.io/badge/code%20of-conduct-ff69b4.svg?style=flat)](https://github.com/tterb/hyde/blob/master/docs/CODE_OF_CONDUCT.md) 
+
+# Simple Dynamic Website
+
+**This Project is one of the challenge in that udemy course, which helps to gain a better knowledge on EJS Module, ExpressJs, NPM Modules as-well.**
+
+### Project Dependency
+
+```javascript
+npm install         // to install all dependencies
+```
+
+### Server
+
+Server file named `server.js`
+
+### PORT - 3000
+
+Here PORT 3000 is being used as default port.
+
+```javascript
+
+node server.js       // command to run the server
+server started!! runs on port 3000
+
+```
+## List of Tabs
+
+* [Default Tab](#default_tab)
+```javascript
+localhost:3000
+```
+* [Compose Tab](#compose_tab)
+```javascript
+localhost:3000/compose      // creating new title & description in home-page, type url to open the page
+```
+
+## Libraries/Modules
+
+* express
+* body-parser
+* ejs
+* lodash
+
+## Thank you
+
+* Thanks to [Dr. Angela Yu](https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/) for the teaching me the concept to become a Full Stack Developer.
+
+### Follow
+
+[![GitHub followers](https://img.shields.io/github/followers/PPranesh?style=social)](https://github.com/PPranesh/My_ToDo_version_1/MyBadges)
+[![Twitter URL](https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Ftwitter.com%2Fpranesh07269610)](https://twitter.com/pranesh07269610)
+[![Twitter Follow](https://img.shields.io/twitter/follow/pranesh07269610?style=social)](https://twitter.com/pranesh07269610)
+
+### Learning 
+
+[![Open Source](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://opensource.org/)
+[![No Maintenance Intended](http://unmaintained.tech/badge.svg)](http://unmaintained.tech/)
+[![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
+
+
+[![forthebadge](https://forthebadge.com/images/featured/featured-built-with-love.svg)](http://forthebadge.com)
---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+
+const app = express();
+
+const homeContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const aboutContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.";
+const contactContent = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+const additionalContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+
+const homeArray = [{ title: "Home", content: homeContent}, { title: "About", content: aboutContent},{ title: "Contact", content: contactContent}, { title: "Additional Content", content: additionalContent}];
+const aboutArray = [{ title: "About", content: aboutContent},{ title: "Contact", content: contactContent}, { title: "Additional Content", content: additionalContent}];
+const contactArray = [{ title: "Contact", content: contactContent}, { title: "About", content: aboutContent}, { title: "Additional Content", content: additionalContent}];
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.render("home",{homeArray});
+});
+
+app.get("/about", function(req, res){
+    res.render("about",{aboutArray});
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact",{contactArray});
+});
+
+app.get("/post", function(req, res){
+    res.render("post");
+});
+
+app.get("/compose", function(req, res){
+    res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+
+    const newContent = {
+        title: _.upperFirst(req.body.inputTitle),
+        content: req.body.inputContent
+    };
+    
+    homeArray.push(newContent);
+
+    res.redirect("/");
+});
+
+app.get("/posts/:postHeading", function(req, res){
+
+    const fromURL = _.lowerCase(req.params.postHeading);
+
+    homeArray.forEach(function(element) {
+
+        const toURL = _.lowerCase(element.title);
+
+        if ( fromURL === toURL ) {
+            res.render("post", {title: element.title, content: element.content });
+        } 
+
+    });
+});
+
+app.listen(3000, function(){
+    console.log("server started! on port 3000");
+});
